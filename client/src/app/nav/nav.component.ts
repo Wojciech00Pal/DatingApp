78fabdc7606f51c4aa3579194bc47d7eda571687
Wojciech00Pal <84765147+Wojciech00Pal@users.noreply.github.com>
2022-11-02 14:31:10 +0100

    End of error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -25,10 +25,6 @@ export class NavComponent implements OnInit {
       console.log(response);
       this.router.navigateByUrl('/members');
 
-    }, error=>
-    {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
