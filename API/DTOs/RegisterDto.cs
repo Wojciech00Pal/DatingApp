dfda6884ca2ae0a76318d59eedf93bd34a4113a9
Wojciech00Pal <84765147+Wojciech00Pal@users.noreply.github.com>
2022-11-24 20:16:40 +0100

    end of reactive forms

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,11 +1,16 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
-        public string UserName { get; set; }
+        [Required]public string UserName { get; set; }
+        [Required] public string KnownAs {get;set;}
+        [Required] public string Gender {get;set;}
+        [Required] public DateTime DateofBirth {get;set;}
+        [Required] public string city {get;set;}
+        [Required] public string country {get;set;}
         [Required]
         [StringLength(8,MinimumLength =4)]
         public string Password { get; set; }
