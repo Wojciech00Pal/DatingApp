58de50387431c0c4cc290e1701366de9faaf5c34
Wojciech00Pal <84765147+Wojciech00Pal@users.noreply.github.com>
2023-03-09 14:31:45 +0100

    Improve on dataTimeSent

--- API/Entities/Message.cs
+++ API/Entities/Message.cs
@@ -13,7 +13,8 @@ namespace API.Entities
         public AppUser recipient { get; set; }
         public string content { get; set; }
         public DateTime? dateRead { get; set; }
-        public DateTime messageSent { get; set; } = DateTime.UtcNow;
+        //public DateTime messageSent { get; set; } = DateTime.UtcNow;
+        public DateTime messageSent { get; set; } = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"));
         public bool senderDelted { get; set; }
         public bool recipientDelted { get; set; }
     }
